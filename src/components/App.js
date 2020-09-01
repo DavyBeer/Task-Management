98418d66389c68eaf2f7f3f36e0e1fbe75b0e87c
Steven <steven1126@swbell.net>
2020-09-01 14:47:24 -0500

    Created router

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,21 @@ import { BrowserRouter, Switch, Redirect, Route } from "react-router-dom";
 import './App.css';
 import NavBar from './NavBar';
 
-function App() {
+const App = () => {
   return (
-    <div>
-      Hello World
-    </div>
+    <BrowserRouter>
+      <div>
+        <NavBar />
+        <Switch>
+          <Redirect exact path="/" to="/about" />
+          <Route path="/about" component={About} />
+          <Route path="/todo" component={Todo} />
+          <Route path="/contact" component={Contact} />
+          <Route component={Error} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
