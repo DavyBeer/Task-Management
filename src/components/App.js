3362208b060c335fc0d7c39ca9d98c7eb1d1fb4f
Steven <steven1126@swbell.net>
2020-09-01 14:45:10 -0500

    Added imports

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import { BrowserRouter, Switch, Redirect, Route } from "react-router-dom";
 import './App.css';
+import NavBar from './NavBar';
 
 function App() {
   return (
