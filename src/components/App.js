ebf49aab8f80d80d3d1631edbdb206698818e214
Steven <steven1126@swbell.net>
2020-09-01 14:57:07 -0500

    Imports

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { BrowserRouter, Switch, Redirect, Route } from "react-router-dom";
 import './App.css';
 import NavBar from './NavBar';
+import About from './About';
+import Todo from './Todo';
+import Contact from './Contacts';
 
 const App = () => {
   return (
