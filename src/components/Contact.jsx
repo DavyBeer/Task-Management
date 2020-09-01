f5baf9c7fd35496bec8d53a18a81ff4652487a66
Steven <steven1126@swbell.net>
2020-09-01 16:13:11 -0500

    Mulitplied input

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,59 @@
 import React from "react";
+import { Prompt } from "react-router-dom";
 
-const Contact = () => {
-  return <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h2>;
-};
+class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: "" };
+  }
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+  handleSubmit() {
+    this.setState({ value: "" });
+  }
+  render() {
+    var style = {
+      width: 300,
+      height: 30,
+      margin: 5
+    };
+    return (
+      <div>
+        <h5>First Name:</h5>
+        <input
+          value={this.state.value}
+          onChange={this.handleChange.bind(this)}
+          style={style}
+        />
+        <h5>Last Name:</h5>
+        <input
+            value={this.state.value}
+            onChange={this.handleChange.bind(this)}
+            style={style}    
+        />
+        <h5>Email:</h5>
+        <input
+            value={this.state.value}
+            onChange={this.handleChange.bind(this)}
+            style={style}    
+        />
+        <h5>Comments:</h5>
+        <input
+            value={this.state.value}
+            onChange={this.handleChange.bind(this)}
+            style={style}    
+        />
+        <div>
+          <button onClick={this.handleSubmit.bind(this)}>Send</button>
+        </div>
+        <Prompt
+          when={this.state.value !== ""}
+          message="are you sure you want to leave without sending a message?"
+        />
+      </div>
+    );
+  }
+}
 
 export default Contact;
