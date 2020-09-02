733e89186403e551aeaf4714abad97198cfcba88
Steven <steven1126@swbell.net>
2020-09-02 16:09:08 -0500

    Fleshed out container

--- src/containers/addButtonContainer.js
+++ src/containers/addButtonContainer.js
@@ -2,3 +2,22 @@ import { connect } from "react-redux";
 import addListItem from "./actions/addListItem.js";
 import AddButton from "./components/AddButton";
 
+const mapStateToProps = state => {
+    return {
+        tasks:state
+    };
+};
+
+const capDispatchToProps = dispatch => {
+    return {
+        onAdd: (task) => {
+            dispatch(addListItem(task));
+        }
+    };
+};
+
+const addButtonContainer = connect(mapStateToProps, mapDispatchToProps)(
+    addButton
+);
+
+export default addButtonContainer;
