7e90fd55348fabdbe4a7e302ddb35c5bf8ffa196
Steven <steven1126@swbell.net>
2020-09-02 14:01:15 -0500

    Created reducers

--- src/reducer/reducer.js
+++ src/reducer/reducer.js
@@ -6,5 +6,51 @@ import {
     MARK_LIST_ITEM
 } from "./constants/constants.js";
 
+const allList = (state = [], action) => {
+    switch (action.type) {
+        case "ADD_LIST_ITEM":
+            return [...state,action.task];
+        case "DEL_LIST_ITEM":
+            return [...state.slice(0,action.index),...state.slice(action.index+1)];
+        case "EDIT_LIST_ITEM":
+            return [...state.slice(0,action.index),...state.slice(action.index)];
+        default:
+            return;
+    }
+};
 
+const activeList = (state = [], action) => {
+    switch (action.type) {
+        case "ADD_LIST_ITEM":
+            return [...state,action.task];
+        case "DEL_LIST_ITEM":
+            return [...state.slice(0,action.index),...state.slice(action.index+1)];
+        case "EDIT_LIST_ITEM":
+            return [...state.slice(0,action.index),...state.slice(action.index)];
+        case "MARK_LIST_ITEM":
+            return [...state.slice(0,action.index),...state.slice(action.index+1)];
+        default:
+            return state;
+    }
+};
 
+const markedList = (state = [], action) => {
+    switch (action.type) {
+        case "MARK_LIST_ITEM":
+            return [...state,action.task];
+        case "DEL_LIST_ITEM":
+            return [...state.slice(0,action.index),...state.slice(action.index+1)];
+        case "EDIT_LIST_ITEM":
+            return [...state.slice(0,action.index),...state.slice(action.index)];
+        default:
+            return state;
+    }
+};
+
+const reducer = combineReducers({
+    allList,
+    activeList,
+    markedList
+});
+
+export default reducer;
