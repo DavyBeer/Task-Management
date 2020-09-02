0614e5125970571b743d963277e705559f3ac32e
Steven <steven1126@swbell.net>
2020-09-02 14:03:46 -0500

    Quotation

--- src/actions/addListItem.js
+++ src/actions/addListItem.js
@@ -2,7 +2,7 @@ import { ADD_LIST_ITEM } from "./constants/constants.js";
 
 const addListItem = (task) => {
     return {
-        type: ADD_LIST_ITEM,
+        type: "ADD_LIST_ITEM",
         item: task
     };
 };
