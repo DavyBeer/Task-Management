38910c32a1fbfb9af18d779d8437a91f740c15ae
Steven <steven1126@swbell.net>
2020-09-03 15:05:12 -0500

    Import filter

--- src/reducer/reducer.js
+++ src/reducer/reducer.js
@@ -2,7 +2,8 @@ import {
     ADD_LIST_ITEM,
     DEL_LIST_ITEM,
     EDIT_LIST_ITEM,
-    MARK_LIST_ITEM
+    MARK_LIST_ITEM,
+    FILTER_LIST_ITEMS
 } from "../constants/constants.js";
 
 const reducer = (state = [], action) => {
