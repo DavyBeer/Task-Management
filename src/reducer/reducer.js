13de0219afae0f9305854b02cbf8e2dc8916c060
Steven <steven1126@swbell.net>
2020-09-03 14:14:04 -0500

    Imported Mark

--- src/reducer/reducer.js
+++ src/reducer/reducer.js
@@ -1,7 +1,8 @@
 import {
     ADD_LIST_ITEM,
     DEL_LIST_ITEM,
-    EDIT_LIST_ITEM
+    EDIT_LIST_ITEM,
+    MARK_LIST_ITEM
 } from "../constants/constants.js";
 
 const reducer = (state = [], action) => {
@@ -18,10 +19,15 @@ const reducer = (state = [], action) => {
                 ...state.slice(0, action.index),
                 ...state.slice(action.index,0)
             ];
+        case MARK_LIST_ITEM:
+            return [
+                ...state.slice(0,action.index),
+                ...state.slice(action.index + 1)
+            ];
       default:
         return state;
     }
-  };
+};
 
 
 
